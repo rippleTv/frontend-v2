0003654e346d6f57660413a5e7ae3570635293e2
Somtozech <ezechinnaemeka@gmail.com>
2020-06-21 21:11:10 +0100

    feat: Added Watch Now Section to Home page

--- components/lib.js
+++ components/lib.js
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 import theme from '../styles/theme';
 import media from '../styles/media';
 
-const Container = styled.section`
+const Container = styled.div`
+	padding: 100px 0;
 	width: 88%;
 	margin: 0 auto;
 	${media.lg`max-width: ${theme.maxWidth}`}
+
+	.fill-height {
+		padding: 0 0;
+	}
 `;
 
 const ButtonLink = styled.a`
--- pages/index.js
+++ pages/index.js
@@ -54,6 +54,63 @@ const Button = styled.a`
 	}
 `;
 
+const WatchNow = styled.section`
+	background: ${({ theme }) => theme.colors.white};
+	color: ${({ theme }) => theme.colors.gray['100']};
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	div {
+		flex: 1;
+	}
+
+	h1 {
+		color: ${({ theme }) => theme.colors.orange};
+		font-size: 30px;
+		text-align: center;
+		margin-bottom: 13px;
+	}
+
+	p {
+		font-size: 18px;
+		text-align: center;
+	}
+
+	.image {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	${media.md`
+		h1 {
+			font-size: 40px;
+		}
+		p {
+			font-size: 20px;
+		}
+	`}
+
+	${media.lg`
+	.container {
+		flex-direction: row;
+	}
+	h1{
+		text-align: left;
+		font-size: 60px;
+	}
+	p {
+		text-align: left;
+		font-size: 26px;
+	}
+	`}
+`;
+
 export default function Home() {
 	return (
 		<div className='container'>
@@ -66,6 +123,21 @@ export default function Home() {
 						<Button>START WATCHING</Button>
 					</HeroContainer>
 				</Hero>
+
+				<WatchNow>
+					<Container className='container'>
+						<div>
+							<h1>Watch everywhere.</h1>
+							<p>
+								Stream unlimited nollywood movies and series on your phone,
+								tablet, laptop, and TV without paying more.
+							</p>
+						</div>
+						<div class='image'>
+							<img src='/images/watchnow.png' />
+						</div>
+					</Container>
+				</WatchNow>
 			</main>
 		</div>
 	);
